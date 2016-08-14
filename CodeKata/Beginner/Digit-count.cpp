33779ebfe947b363179d3ff5837143efaa7f3243
Krish Munot <krishmunot@gmail.com>
2016-08-14 15:17:35 +0530

    digi count

---
+++ CodeKata/Beginner/Digit-count.cpp
@@ -0,0 +1,19 @@
+/*
+* 10 - Count number of digits of an integer.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int num,count=0;
+    cin>>num;
+	while(num>0)
+	{
+		num/=10;
+		count+=1;
+	}
+	cout<<count<<endl;
+	return 0;
+}
