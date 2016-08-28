485dbd86792e23666d77db23c5a62fa8547a5c09
Krish Munot <krishmunot@gmail.com>
2016-08-28 22:39:10 +0530

    Update Digit-count.cpp

--- CodeKata/Beginner/Digit-count.cpp
+++ CodeKata/Beginner/Digit-count.cpp
@@ -5,15 +5,13 @@
 #include <iostream>
 using namespace std;
 
-int main() 
-{
+int main() {
 	int num,count=0;
-    cin>>num;
-	while(num>0)
-	{
-		num/=10;
-		count+=1;
-	}
-	cout<<count<<endl;
+    	cin >> num;
+	while(num > 0) {
+	    num/ = 10;
+	    count+ = 1;
+	} 
+	cout << count << endl;
 	return 0;
 }
