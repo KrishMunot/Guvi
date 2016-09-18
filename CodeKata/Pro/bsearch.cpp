a9eda430852e9b92d35993e7b702a7ffc1b5c111
Krish Munot <krishmunot@gmail.com>
2016-09-18 22:37:57 +0000

    Update bsearch.cpp

--- CodeKata/Pro/bsearch.cpp
+++ CodeKata/Pro/bsearch.cpp
@@ -1,53 +1,44 @@
-#include<stdio.h>
-#include <limits.h>
-int max(int a, int b) 
-{ 
-return (a > b)? a : b; 
-}
+#include<bits/stdc++.h>
 
+int max(int a, int b) { 
+ return (a > b)? a : b; 
+}
 int max(int a, int b, int c) { return max(max(a, b), c); }
- 
-int maxCrossingSum(int arr[], int l, int m, int h)
+int maxCrossSum(int a[], int l, int m, int h)
 {
-   
-    int sum = 0;
-    int left_sum = INT_MIN;
-    for (int i = m; i >= l; i--)
-    {
-        sum = sum + arr[i];
-        if (sum > left_sum)
-          left_sum = sum;
+   int sum = 0;
+   int left_sum = INT_MIN;
+   for (int i = m; i >= l; i--)
+   {
+        sum = sum + a[i];
+        if (sum > l_sum)
+          l_sum = sum;
     }
- 
-    
     sum = 0;
-    int right_sum = INT_MIN;
+    int r_sum = INT_MIN;
     for (int i = m+1; i <= h; i++)
     {
         sum = sum + arr[i];
-        if (sum > right_sum)
-          right_sum = sum;
+        if (sum > r_sum)
+          r_sum = sum;
     }
- 
-    
-    return left_sum + right_sum;
+    return l_sum + r_sum;
 }
-int maxSubArraySum(int arr[], int l, int h)
+int maxSubSum(int a[], int l, int h)
 {
-
    if (l == h)
      return arr[l];
    int m = (l + h)/2;
-   return max(maxSubArraySum(arr, l, m),
-              maxSubArraySum(arr, m+1, h),
-              maxCrossingSum(arr, l, m, h));
+   return max(maxSubSum(a, l, m),
+              maxSubSum(a, m+1, h),
+              maxCrossSum(a, l, m, h));
 }
+
 int main()
 {
-   int arr[] = {2, 3, 4, 5, 7};
-   int n = sizeof(arr)/sizeof(arr[0]);
-   int max_sum = maxSubArraySum(arr, 0, n-1);
-   printf("Maximum contiguous sum is %d\n", max_sum);
-   getchar();
+   int a[] = {2, 3, 4, 5, 7};
+   int n = sizeof(a)/sizeof(a[0]);
+   int max_sum = maxSubSum(arr, 0, n-1);
+   printf("Max continuous sum is %d\n", max_sum);
    return 0;
 }
